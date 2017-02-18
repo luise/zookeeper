cbf48823c2ad20f2a10342a0805903e938010028
Luise Valentin <valentin.luise@gmail.com>
2017-02-17 18:49:10 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ZooKeeper for Quilt.js
+
+This repository implements a ZooKeeper specification for Quilt.js.  See
+[Quilt](http://quilt.io) for more information.
---
+++ zookeeper-example.js
@@ -0,0 +1,17 @@
+var zookeeper = require("github.com/quilt/zookeeper");
+
+var n = 3;
+var zoo = new zookeeper.Zookeeper(n);
+var deployment = createDeployment({});
+
+var baseMachine = new Machine({
+    provider: "Amazon",
+    region: "us-west-1",
+    size: "m4.large",
+    diskSize: 32,
+    sshKeys: githubKeys("ejj"), // Replace with your GitHub username.
+});
+
+deployment.deploy(baseMachine.asMaster())
+deployment.deploy(baseMachine.asWorker().replicate(n))
+deployment.deploy(zoo);
