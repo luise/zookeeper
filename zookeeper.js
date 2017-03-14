1465c29bc1fab4a43bf27f0abfd13b740f081c9b
Kevin Lin <kaikai526@gmail.com>
2017-03-14 09:41:23 -0500

    lib: Remove custom Docker image
    Rather than generate the zookeeper config at runtime, we now simply
    generate it at compile time.

--- zookeeper.js
+++ zookeeper.js
@@ -1,25 +1,39 @@
-var image = "quilt/zookeeper";
+var image = "jplock/zookeeper:3.4.8";
+var dataDir = "/tmp/zookeeper";
 
 function Zookeeper(n) {
     var cns = new Container(image)
         .replicate(n);
     this.zoo = new Service("zookeeper", cns);
 
-    var allMembers = [];
+    var cluster = {};
     for (var i = 0; i < cns.length; i++) {
-        cns[i].setHostname("zk" + (i+1));
-        allMembers.push(cns[i].getHostname());
+        cns[i].setHostname("zk" + i)
+        cluster[i.toString()] = cns[i].getHostname();
     }
 
-    var allMembersStr = allMembers.join(",");
+    var cfg = buildConfig(cluster);
     for (var i = 0; i < cns.length; i++) {
-        cns[i].command = ["" + (i + 1), allMembersStr];
+        cns[i].filepathToContent["/opt/zookeeper/conf/zoo.cfg"] = cfg;
+        cns[i].filepathToContent[dataDir + "/myid"] = i.toString();
     }
 
     this.zoo.connect(new PortRange(1000, 65535), this.zoo);
     this.deploy = function(deployment) {
         deployment.deploy(this.zoo);
     }
+}
+
+function buildConfig(cluster) {
+    var cfg = "tickTime=2000\n" +
+              "dataDir=" + dataDir + "\n" +
+              "clientPort=2181\n" +
+              "initLimit=5\n" +
+              "syncLimit=2\n"
+    for (id in cluster) {
+        cfg += "server." + id + "=" + cluster[id] + ":2888:3888\n";
+    }
+    return cfg;
 }
 
 exports.Zookeeper = Zookeeper;
