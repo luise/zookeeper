31292c20281b2435ebaea30e29e82058f85c760d
Kevin Lin <kaikai526@gmail.com>
2017-03-14 10:16:45 -0500

    lib: Properly assign hostnames
    The zookeeper spec relied on a correspondence between the order of
    hostnames returned by `Service.children`, and the order the QRI assigned
    hostnames to the containers within a service. This assumption is no
    longer safe, so this patch migrates us to the new `setHostname`
    function, which allows us to set the hostname of a container at
    compile time.

--- zookeeper.js
+++ zookeeper.js
@@ -4,9 +4,16 @@ function Zookeeper(n) {
     var cns = new Container(image)
         .replicate(n);
     this.zoo = new Service("zookeeper", cns);
-    var hostnames = this.zoo.children();
+
+    var allMembers = [];
     for (var i = 0; i < cns.length; i++) {
-        cns[i].command = ["" + (i + 1), hostnames.join(",")];
+        cns[i].setHostname("zk" + (i+1));
+        allMembers.push(cns[i].getHostname());
+    }
+
+    var allMembersStr = allMembers.join(",");
+    for (var i = 0; i < cns.length; i++) {
+        cns[i].command = ["" + (i + 1), allMembersStr];
     }
 
     this.zoo.connect(new PortRange(1000, 65535), this.zoo);
